f89efff964b76be09d0bd1eabde84ac98de667e7
sandubhai36 <sohelmirza7u7@gmail.com>
2024-08-12 14:09:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -216,7 +216,4 @@ def main():
     application.add_handler(CallbackQueryHandler(button))
     application.add_handler(CommandHandler('add_promocode', add_promocode))
     application.add_handler(CommandHandler('subscribe', subscribe))
-    application.add_handler(CommandHandler('show_keys', show_keys))
-    application.add_handler(CommandHandler('upload_promocodes', upload_promocodes, filters.Document.ALL))
-    application.add_handler(CommandHandler('feedback', feedback))
-    application.add
+    application.add_handler(Command
